35d5f73af6c901ac6af81e53ea615c810793e935
x4v1zzz0-crypto <x4v1zzz0@gmail.com>
2026-01-31 21:25:47 -0300

    Change export syntax from default to CommonJS

--- api/request.js
+++ api/request.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
   const { paypal, userId } = req.body;
 
   global.sentUsers = global.sentUsers || [];
@@ -29,4 +29,4 @@ export default async function handler(req, res) {
   });
 
   res.status(200).json({ ok: true });
-}
+};
